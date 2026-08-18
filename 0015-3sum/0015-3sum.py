596817603e50134ed4fefd26020858bc5b92035c
Bernice Malaiarasu <163750603+bernice511@users.noreply.github.com>
2026-08-19 00:12:35 +0530

    Time: 661 ms (33.7%), Space: 23.7 MB (5.35%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -3,8 +3,11 @@ class Solution:
         sol = set()
         nums.sort()
         for i in range(len(nums)-1):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
             left = i+1
             right = len(nums)-1
+            
             while left<right:
                 total =  nums[i]+nums[left]+nums[right]
                 if total == 0:
